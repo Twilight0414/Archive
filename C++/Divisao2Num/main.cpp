8f55102b7880972af5145e99b4ef21e2ae5062d3
Thales C. Santos <114881155+Twilight0414@users.noreply.github.com>
2024-11-18 13:56:58 -0300

    Add files via upload

---
+++ C++/Divisao2Num/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+  int num1, num2, div;
+
+  cout << "Digite o primeiro numero: \n";
+  cin >> num1;
+  cout << "Digite p  segundo numero: \n";
+  cin >> num2;
+
+  div = num1 / num2;
+  cout << "O resultado da divisão é: " << div << "\n";
+}
