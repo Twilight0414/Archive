fbd5e1bf0866482f391ab461f5915ef401aac04d
Thales C. Santos <114881155+Twilight0414@users.noreply.github.com>
2024-11-18 13:53:56 -0300

    Add files via upload

---
+++ C++/Multiplicacao3Num/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+  float num1, num2, num3, mult;
+
+  cout << "Digite o numero 1: \n";
+  cin >> num1;
+  cout << "Digite o numero 2: \n";
+  cin >> num2;
+  cout << "Digite o numero 3: \n";
+  cin >> num3;
+
+  mult = num1 * num2 * num3;
+
+  cout << "O resultado da multiplicação é: " << mult << "\n";
+}
